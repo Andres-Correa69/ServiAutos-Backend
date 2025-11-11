944df906943971b36dbd9a3aaf223994c90a12ea
Andres-Correa69 <correavalencia334@gmail.com>
2025-11-11 00:28:44 -0500

    test postman

--- src/main/java/co/edu/uniquindio/serviautosbackend/config/SecurityConfig.java
+++ src/main/java/co/edu/uniquindio/serviautosbackend/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:4200",
                 "http://localhost:8080",  // Para Postman en local
-                "https://servi-autos-frontend.vercel.app"
+                "https://servi-autos-frontend.vercel.app",
+                "*"
         ));
         
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
